6fce63d1c0025cf09c7100aae6e7ba4672129169
hung <quochung@simicart.com>
2022-12-01 11:17:53 +0700

    update code

---
+++ awesome-links/data/links.ts
@@ -0,0 +1,31 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    imageUrl: "https://nextjs.org/static/twitter-cards/home.jpg",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    imageUrl: "https://www.prisma.io/images/og-image.png",
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    imageUrl:
+      "https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  },
+  {
+    category: "Open Source",
+    description: "GraphQL implementation ",
+    imageUrl: "https://www.apollographql.com/apollo-home.jpg",
+    title: "Apollo GraphQL",
+    url: "https://apollographql.com",
+  },
+];
---
+++ awesome-links/graphql/types/Link.ts
@@ -0,0 +1,43 @@
+// /graphql/types/Link.ts
+import { objectType, extendType, intArg, stringArg } from "nexus";
+import { connectionFromArraySlice, cursorToOffset } from 'graphql-relay';
+import { User } from "./User";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.string("id");
+    t.string("title");
+    t.string("url");
+    t.string("description");
+    t.string("imageUrl");
+    t.string("category");
+  },
+});
+
+export const LinksQuery = extendType({
+    type: 'Query',
+    definition(t) {
+      t.connectionField('links', {
+        type: Link,
+        resolve: async (_, { after, first }, ctx) => {
+          const offset = after ? cursorToOffset(after) + 1 : 0;
+          if (isNaN(offset)) throw new Error('cursor is invalid');
+  
+          const [totalCount, items] = await Promise.all([
+            ctx.prisma.link.count(),
+            ctx.prisma.link.findMany({
+              take: first,
+              skip: offset,
+            }),
+          ]);
+  
+          return connectionFromArraySlice(
+            items,
+            { first, after },
+            { sliceStart: offset, arrayLength: totalCount }
+          );
+        },
+      });
+    },
+  });
