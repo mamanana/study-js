7a212f3f0e2d441e5bc0b57f7b4d86aef9dc5ff2
hung <quochung@simicart.com>
2023-03-31 13:50:18 +0700

    update

--- nextjs/graphql/types/User.ts
+++ nextjs/graphql/types/User.ts
@@ -1,9 +1,9 @@
 import { builder } from "../builder";
-import prisma from "../../lib/prisma"
+import prisma from "../../lib/prisma";
 
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const APP_SECRET = 'GraphQL-is-aw3some';
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const APP_SECRET = "GraphQL-is-aw3some";
 
 builder.prismaObject("User", {
   fields: (t) => ({
@@ -36,8 +36,7 @@ builder.mutationField("signup", (t) =>
       password: t.arg.string({ required: true }),
     },
     resolve: async (parent, args) => {
-
-      const password = await bcrypt.hash(args.password, 10)
+      const password = await bcrypt.hash(args.password, 10);
 
       const user = await prisma.user.create({
         data: { ...args, password },
@@ -45,6 +44,36 @@ builder.mutationField("signup", (t) =>
 
       const token = jwt.sign({ userId: user.id }, APP_SECRET);
       // console.log(args)
+      return {
+        token,
+        user,
+      };
+    },
+  })
+);
+
+builder.mutationField("login", (t) =>
+  t.field({
+    type: AuthPayload,
+    args: {
+      email: t.arg.string({ required: true }),
+      password: t.arg.string({ required: true }),
+    },
+    resolve: async (parent, args) => {
+      const user = await prisma.user.findUnique({
+        where: { email: args.email },
+      });
+      if (!user) {
+        throw new Error("No such user found");
+      }
+
+      const valid = await bcrypt.compare(args.password, user.password);
+      if (!valid) {
+        throw new Error("Invalid password");
+      }
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
       return {
         token,
         user,
